ec057db24d1fba030738bcb718e164e5cd65d50c
Amilkar Munoz <yellowballpython@gmail.com>
2021-09-29 01:10:52 -0600

    add motion elements

---
+++ src/components/motion/components.js
@@ -0,0 +1,18 @@
+import {
+    Flex,
+    Stack,
+    VStack,
+    HStack,
+    Box,
+    Divider
+} from '@chakra-ui/react'
+import {
+    motion
+} from 'framer-motion'
+
+export const MotionBox = motion(Box)
+export const MotionFlex = motion(Flex)
+export const MotionStack = motion(Stack)
+export const MotionVStack = motion(VStack)
+export const MotionHStack = motion(HStack)
+export const MotionDivider = motion(Divider)
